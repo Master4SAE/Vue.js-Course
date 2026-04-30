bd4603a3fc1091057ff064103cdcdde50e0833b7
Sohaib Ebrahimi <ebrahimi_sa@icloud.com>
2026-04-30 18:35:30 +0300

    first course completed

--- MyFirstVueCourse/src/App.vue
+++ MyFirstVueCourse/src/App.vue
@@ -1,11 +1,28 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const name = ref("Sohaib");
+const href = ref("https://vuejs.org/")
+const myButtonDisable = ref(false)
+const codingYears = ref(3)
+const portfolio = ref("https://my-portfolio-d54ab.web.app")
+const blockLeave = () => {
+  alert("You cant leave this page!");
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Hello my name is <a v-on:click=[blockLeave()] v-bind:href="portfolio" target="_blank">{{ name }}</a></h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    Visit 
+    <a v-bind:href="href" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p>
+    {{
+      (codingYears > 0 ? "I have been coding for " + codingYears + " years " : "I am a noob") 
+    }} </p>
+  <button v-on:click="codingYears++" v-bind:disabled="myButtonDisable"> Click me </button>
 </template>
 
 <style scoped></style>
